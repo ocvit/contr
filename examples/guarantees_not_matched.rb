9536cc440da74826b507e49c115be35d52a0a2bd
Dmytro Horoshko <electric.molfar@gmail.com>
2024-03-11 14:41:46 +0200

    docs: update examples details

--- examples/guarantees_not_matched.rb
+++ examples/guarantees_not_matched.rb
@@ -33,53 +33,61 @@ contract = SumContract.new
 contract.check(*args) { args.inject(:+) }
 # => Contr::Matcher::GuaranteesNotMatched: failed rules: [[:guarantee, :result_is_positive, :failed]], args: [1, -2.0]
 
-# [if previous dump doesn't exist]
 #
-# state dumped to: /tmp/contracts/SumContract/2850248.dump
+# [======= if previous dump doesn't exist =======]
+#
 # state:
-# {
-#   ts:            "2024-03-11T09:23:04.245Z",
-#   contract_name: "SumContract",
-#   failed_rules:  [{type: :guarantee, name: :result_is_positive, status: :failed}],
-#   ok_rules:      [{type: :guarantee, name: :result_is_float, status: :ok}],
-#   async:         false,
-#   args:          [1, -2.0],
-#   result:        -1.0
-# }
+#   {
+#     ts:            "2024-03-11T09:23:04.245Z",
+#     contract_name: "SumContract",
+#     failed_rules:  [{type: :guarantee, name: :result_is_positive, status: :failed}],
+#     ok_rules:      [{type: :guarantee, name: :result_is_float, status: :ok}],
+#     async:         false,
+#     args:          [1, -2.0],
+#     result:        -1.0
+#   }
+#
+# dumped to:
+#   /tmp/contracts/SumContract/2850248.dump
 #
 # log:
-# D, [2024-03-11T11:23:04.245811 #71262] DEBUG -- : {"ts":"2024-03-11T09:23:04.245Z","contract_name":"SumContract","failed_rules":[{"type":"guarantee","name":"result_is_positive","status":"failed"}],"ok_rules":[{"type":"guarantee","name":"result_is_float","status":"ok"}],"async":false,"args":[1,-2.0],"result":-1.0,"dump_info":{"path":"/tmp/contracts/SumContract/2850248.dump"},"tag":"contract-failed"}
-
-# [if previous dump exists]
+#   D, [2024-03-11T11:23:04.245811 #71262] DEBUG -- : {"ts":"2024-03-11T09:23:04.245Z","contract_name":"SumContract","failed_rules":[{"type":"guarantee","name":"result_is_positive","status":"failed"}],"ok_rules":[{"type":"guarantee","name":"result_is_float","status":"ok"}],"async":false,"args":[1,-2.0],"result":-1.0,"dump_info":{"path":"/tmp/contracts/SumContract/2850248.dump"},"tag":"contract-failed"}
 #
-# state is not dumped once again (10 minutes window by default), but still logged - without `dump_info` field this time
+#
+# [======= if previous dump exists =======
+#
+# state is not dumped once again, but still logged - without `dump_info` field
 #
 # log:
-# D, [2024-03-11T11:23:04.245811 #71262] DEBUG -- : {"ts":"2024-03-11T09:23:04.245Z","contract_name":"SumContract","failed_rules":[{"type":"guarantee","name":"result_is_positive","status":"failed"}],"ok_rules":[{"type":"guarantee","name":"result_is_float","status":"ok"}],"async":false,"args":[1,-2.0],"result":-1.0,"tag":"contract-failed"}
+#   D, [2024-03-11T11:23:04.245811 #71262] DEBUG -- : {"ts":"2024-03-11T09:23:04.245Z","contract_name":"SumContract","failed_rules":[{"type":"guarantee","name":"result_is_positive","status":"failed"}],"ok_rules":[{"type":"guarantee","name":"result_is_float","status":"ok"}],"async":false,"args":[1,-2.0],"result":-1.0,"tag":"contract-failed"}
 
 contract.check_async(*args) { args.inject(:+) }
 # => -1.0
 
-# [if previous dump doesn't exist]
 #
-# state dumped to: /tmp/contracts/SumContract/2850248.dump
+# [======= if previous dump doesn't exist =======]
+#
 # state:
-# {
-#   ts:            "2024-03-11T09:23:04.245Z",
-#   contract_name: "SumContract",
-#   failed_rules:  [{type: :guarantee, name: :result_is_positive, status: :failed}],
-#   ok_rules:      [{type: :guarantee, name: :result_is_float, status: :ok}],
-#   async:         true,
-#   args:          [1, -2.0],
-#   result:        -1.0
-# }
+#   {
+#     ts:            "2024-03-11T09:23:04.245Z",
+#     contract_name: "SumContract",
+#     failed_rules:  [{type: :guarantee, name: :result_is_positive, status: :failed}],
+#     ok_rules:      [{type: :guarantee, name: :result_is_float, status: :ok}],
+#     async:         true,
+#     args:          [1, -2.0],
+#     result:        -1.0
+#   }
+#
+# dumped to:
+#   /tmp/contracts/SumContract/2850248.dump
 #
 # log:
-# D, [2024-03-11T11:23:04.245811 #71262] DEBUG -- : {"ts":"2024-03-11T09:23:04.245Z","contract_name":"SumContract","failed_rules":[{"type":"guarantee","name":"result_is_positive","status":"failed"}],"ok_rules":[{"type":"guarantee","name":"result_is_float","status":"ok"}],"async":true,"args":[1,-2.0],"result":-1.0,"dump_info":{"path":"/tmp/contracts/SumContract/2850248.dump"},"tag":"contract-failed"}
-
-# [if previous dump exists]
+#   D, [2024-03-11T11:23:04.245811 #71262] DEBUG -- : {"ts":"2024-03-11T09:23:04.245Z","contract_name":"SumContract","failed_rules":[{"type":"guarantee","name":"result_is_positive","status":"failed"}],"ok_rules":[{"type":"guarantee","name":"result_is_float","status":"ok"}],"async":true,"args":[1,-2.0],"result":-1.0,"dump_info":{"path":"/tmp/contracts/SumContract/2850248.dump"},"tag":"contract-failed"}
 #
-# state is not dumped once again (10 minutes window by default), but still logged - without `dump_info` field this time
+#
+# [======= if previous dump exists =======
+#
+# state is not dumped once again, but still logged - without `dump_info` field
 #
 # log:
-# D, [2024-03-11T11:23:04.245811 #71262] DEBUG -- : {"ts":"2024-03-11T09:23:04.245Z","contract_name":"SumContract","failed_rules":[{"type":"guarantee","name":"result_is_positive","status":"failed"}],"ok_rules":[{"type":"guarantee","name":"result_is_float","status":"ok"}],"async":true,"args":[1,-2.0],"result":-1.0,"tag":"contract-failed"}
+#   D, [2024-03-11T11:23:04.245811 #71262] DEBUG -- : {"ts":"2024-03-11T09:23:04.245Z","contract_name":"SumContract","failed_rules":[{"type":"guarantee","name":"result_is_positive","status":"failed"}],"ok_rules":[{"type":"guarantee","name":"result_is_float","status":"ok"}],"async":true,"args":[1,-2.0],"result":-1.0,"tag":"contract-failed"}
